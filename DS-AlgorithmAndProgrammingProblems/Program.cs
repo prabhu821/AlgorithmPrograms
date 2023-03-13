db1a4d4a7c4aabfe6c880e6570a2f55117c64b24
rishiraj007 <rishiraj@mounttalent.com>
2023-03-13 17:33:36 +0530

    [Prabhu] Add: DS Algorithm and Programming Problem

---
+++ DS-AlgorithmAndProgrammingProblems/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DS_AlgorithmAndProgrammingProblems;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("DS-Algorithm and Programming Problems");
+    }
+}
