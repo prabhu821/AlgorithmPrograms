8f8408110a80c60f4e31a2bf9827da3cad48ef11
rishiraj007 <rishiraj@mounttalent.com>
2023-03-13 17:45:49 +0530

    [Prabhu] Add: Binary Search

---
+++ DS-AlgorithmAndProgrammingProblems/AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace DS_AlgorithmAndProgrammingProblems.AlgorithmPrograms;
+
+public class BinarySearch
+{
+    string[] binaryArray;
+    public void ReadTextFile(string filepath)
+    {
+        string words = File.ReadAllText(filepath);
+        binaryArray = words.Split(",");
+    }
+    public void BinarySearchOperation(string input)
+    {
+        foreach (var words in binaryArray)
+        {
+            if (words.Equals(input))
+            {
+                Console.WriteLine("Found the word in the Word file");
+                return;
+            }
+        }
+        Console.WriteLine("Element not available in the list of words");
+    }
+}
--- DS-AlgorithmAndProgrammingProblems/Program.cs
+++ DS-AlgorithmAndProgrammingProblems/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace DS_AlgorithmAndProgrammingProblems;
+﻿using DS_AlgorithmAndProgrammingProblems.AlgorithmPrograms;
+
+namespace DS_AlgorithmAndProgrammingProblems;
 
 public class Program
 {
+    public static string filepath = @"D:\OneDrive\Desktop\bridgelabz\DS-AlgorithmAndProgrammingProblems\DS-AlgorithmAndProgrammingProblems\AlgorithmPrograms\WordFile.txt";
     static void Main(string[] args)
     {
         Console.WriteLine("DS-Algorithm and Programming Problems");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nSelect option to execute \n1.Binary Search \n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearch search = new BinarySearch();
+                    search.ReadTextFile(filepath);
+                    Console.WriteLine("Enter word you want to search");
+                    string str = Console.ReadLine();
+                    search.BinarySearchOperation(str);
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
